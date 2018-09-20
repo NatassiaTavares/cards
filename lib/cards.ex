b91def60ce1312b35465a55ed4470034bc653c1a
Natassia Tavares <natassiaxtavares@gmail.com>
2018-09-19 22:38:10 -0300

    Added create_deck and shuffle methods

--- lib/cards.ex
+++ lib/cards.ex
@@ -12,7 +12,11 @@ defmodule Cards do
       :world
 
   """
-  def hello do
-    :world
+  def  create_deck do
+    ["Ace", "Spade", "Hearts"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
   end
 end
